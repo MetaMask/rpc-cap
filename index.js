d394dbf4a0fa4fc077fd04c894f0b901aa5d1496
Dan Finlay <dan@danfinlay.com>
2019-01-03 22:18:47 -0800

    one test failing left

--- index.js
+++ index.js
@@ -1,2 +1,87 @@
-class EthLoginController {}
+class EthLoginController {
+
+  constructor({ origin = '', safeMethods = [], permissions = {}, methods = {}}) {
+    this.origin = origin
+    this.safeMethods = safeMethods
+    this.permissions = permissions
+    this.methods = methods
+  }
+
+  serialize () {
+    const { origin, safeMethods, permissions, accounts } = this
+    return { origin, safeMethods, permissions, accounts }
+  }
+
+  grantNewPermissions () {
+
+  }
+
+  /*
+   * The method by which middleware filters which methods
+   * meet its requirements or not.
+   */
+  async requestMethod(req, res, next) {
+    const permission = this.permissions[req.method]
+    if (!permission) {
+      res.error = 'Origin unauthorized to use ' + req.method
+      throw new Error(res.error)
+    }
+
+    const { prereq } = permission
+    if (prereq) {
+      const met = prereq(req, res)
+      if (!met) {
+        res.error = 'Failed to authorize ' + req.method
+        throw new Error(res.error)
+      }
+    }
+    res = await this._callMethod(req, res, next)
+  }
+
+  async _callMethod(method, params, next) {
+    if (method in this.methods) {
+      return await this.methods[method](params)
+    } else {
+      if (next) {
+        next()
+      } else {
+        throw new Error('Method not found and next unavailable.')
+      }
+    }
+  }
+
+  providerMiddlewareFunction (req, res, next, end) {
+    const method = req.method
+
+    // skip registered safe/passthrough methods
+    if (this.safeMethods.includes(method)) {
+      return next()
+    }
+
+    if (method in this.methods) {
+      this.requestMethod(req, res, next)
+      .then((result) => {
+        res.result = result
+        return end()
+      })
+      .catch((reason) => {
+        return end(new Error('Unable to provide the requested permissions.\n' + reason))
+      })
+    } else {
+      return next()
+    }
+  }
+
+  async completePrereqs(permissions) {
+    for (let i = 0; i < permissions.length; i++) {
+      const met = await permissions[i].prereq()
+      if (!met) {
+        throw new Error('Request prerequisite not met.')
+      }
+    }
+
+    return permissions
+  }
+}
+
 module.exports = EthLoginController
--- test/index.js
+++ test/index.js
@@ -1,2 +1,114 @@
 const test = require('tape')
-const Module = require('../')
+const LoginController = require('../')
+
+test('setup test', async (t) => {
+  const WRITE_RESULT = 'impeccable result'
+
+  const ctrl = new LoginController({
+    origin: 'login.metamask.io',
+
+    // safe methods never require approval,
+    // are considered trivial / no risk.
+    // maybe reading should be a permission, though!
+    safeMethods: ['eth_read'],
+
+    permissions: {
+      'eth_write': {
+        method: 'eth_write',
+        prereq: () => Promise.resolve(true),
+      }
+    },
+
+    methods: {
+      'eth_write': () => Promise.resolve(WRITE_RESULT)
+    }
+  })
+
+  try {
+    let result = await ctrl._callMethod('eth_write', {})
+    t.equal(result, WRITE_RESULT, 'write result returned')
+  } catch (error) {
+    t.error(reason, 'want no error')
+    t.end(reason)
+  }
+
+  let req = { method: 'eth_write' }
+  let res= { foo: 'bar' }
+  ctrl.providerMiddlewareFunction(req, res, next, end)
+
+  function next() {
+    t.ok(true, 'next was called')
+    t.end()
+  }
+
+  function end(reason) {
+    t.error(reason, 'error thrown')
+    t.end(reason)
+  }
+
+})
+
+test('#_callMethod with approved prereqs with no method throw error', async (t) => {
+  const WRITE_RESULT = 'impeccable result'
+
+  const ctrl = new LoginController({
+    origin: 'login.metamask.io',
+
+    // safe methods never require approval,
+    // are considered trivial / no risk.
+    // maybe reading should be a permission, though!
+    safeMethods: ['eth_read'],
+
+    permissions: {
+      'eth_write': {
+        method: 'eth_write',
+        prereq: () => Promise.resolve(true),
+      },
+      'eth_write2': {
+        method: 'eth_write2',
+        prereq: () => Promise.resolve(true),
+    }},
+
+    methods: {
+      'eth_write': () => Promise.resolve(WRITE_RESULT)
+    }
+  })
+
+  let result = await ctrl._callMethod('eth_write2', {}, () => {
+    t.ok(true, 'next called')
+    t.end()
+  })
+
+})
+
+test('#providerMiddlewareFunction, approved prereqs with no method pass through', async (t) => {
+  const WRITE_RESULT = 'impeccable result'
+
+  const ctrl = new LoginController({
+    origin: 'login.metamask.io',
+
+    safeMethods: ['eth_read'],
+
+    permissions: {
+      'eth_write2': {
+        method: 'eth_write2',
+        prereq: () => Promise.resolve(true),
+    }},
+
+  })
+
+  let req = { method: 'eth_write' }
+  let res= { foo: 'bar' }
+  ctrl.providerMiddlewareFunction(req, res, next, end)
+
+  function next() {
+    t.ok(true, 'passed through')
+    t.end()
+  }
+
+  function end(reason) {
+    t.error(reason)
+    t.end(reason)
+  }
+
+})
