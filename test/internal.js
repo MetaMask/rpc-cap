b67a481627248c6c9a96c73133c8e7e555dca4d4
Dan Finlay <dan@danfinlay.com>
2019-01-08 23:57:46 -0800

    Fix get method

--- test/internal.js
+++ test/internal.js
@@ -6,7 +6,6 @@ const equal = require('fast-deep-equal')
 // Maybe submit to https://github.com/ethereum/wiki/wiki/JSON-RPC-Error-Codes-Improvement-Proposal
 const USER_REJECTION_CODE = 5
 
-
 test('getPermissions with none returns empty object', async (t) => {
   const expected = {}
 
@@ -28,7 +27,40 @@ test('getPermissions with none returns empty object', async (t) => {
     t.ok(equal(res.result, expected), 'should be equal')
     t.end()
   }
-
 })
 
+test('getPermissions with some returns them', async (t) => {
+  const expected = {
+    'restricted': {},
+    'restricted2': { foo: 'bar' }
+  }
+
+  const ctrl = new LoginController({
+    initState: {
+      domains: {
+        'login.metamask.io': {
+          permissions: expected,
+        },
+      }
+    }
+  })
+
+  const domain = 'login.metamask.io'
+  let req = { method: 'getPermissions' }
+  let res = {}
+
+  ctrl.providerMiddlewareFunction(domain, req, res, next, end)
+
+  function next() {
+    t.ok(false, 'next should not be called')
+    t.end()
+  }
+
+  function end(reason) {
+    t.error(reason, 'error thrown')
+    console.log(res.result, expected)
+    t.ok(equal(res.result, expected), 'should be equal')
+    t.end()
+  }
+})
 
