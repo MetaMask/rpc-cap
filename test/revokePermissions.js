940a3f122730446ed045f0d7c5e3424ce6d5cdb2
Dan Finlay <dan@danfinlay.com>
2019-01-10 19:47:29 -0800

    Add revoke permissions test

---
+++ test/revokePermissions.js
@@ -0,0 +1,120 @@
+const test = require('tape')
+const LoginController = require('../')
+const equal = require('fast-deep-equal')
+
+// TODO: Standardize!  Maybe submit to https://github.com/ethereum/wiki/wiki/JSON-RPC-Error-Codes-Improvement-Proposal
+const USER_REJECTION_CODE = 5
+
+test('revokePermissions on granted permission deletes that permission', async (t) => {
+
+  const expected = {}
+
+  const ctrl = new LoginController({
+    initState: {
+      "domains": {
+        "login.metamask.io": {
+          "permissions": {
+            "restricted": {
+              "date": "0"
+            }
+          }
+        },
+        "other.domain.com": {
+          "permissions": {
+            "restricted": {
+              "date": 1547176021698,
+              "grantedBy": "login.metamask.io"
+            }
+          }
+        }
+      }
+    }
+  })
+
+  const domain = 'login.metamask.io'
+  const otherDomain = 'other.domain.com'
+  let req = {
+    method: 'revokePermissions',
+    params: [
+      otherDomain,
+      {
+        'restricted': {},
+      }
+    ]
+  }
+  let res = {}
+
+  ctrl.providerMiddlewareFunction(domain, req, res, next, end)
+
+  function next() {
+    t.ok(false, 'next should not be called')
+    t.end()
+  }
+
+  function end(reason) {
+    t.error(reason, 'error should not be thrown')
+    t.ok(equal(ctrl._getPermissions(otherDomain), expected), 'should have no permissions now')
+    t.ok(ctrl._getPermissions(domain), 'should have permissions still')
+    t.end()
+  }
+})
+
+
+test('revokePermissions on unrelated permission returns auth error', async (t) => {
+
+  const expected = {}
+
+  const ctrl = new LoginController({
+    initState: {
+      "domains": {
+        "login.metamask.io": {
+          "permissions": {
+            "restricted": {
+              "date": "0"
+            }
+          }
+        },
+        "other.domain.com": {
+          "permissions": {
+            "restricted": {
+              "date": 1547176021698,
+              "grantedBy": "unrelated.domain.co"
+            }
+          }
+        }
+      }
+    }
+  })
+
+  const domain = 'login.metamask.io'
+  const otherDomain = 'other.domain.com'
+  let req = {
+    method: 'revokePermissions',
+    params: [
+      otherDomain,
+      {
+        'restricted': {},
+      }
+    ]
+  }
+  let res = {}
+
+  ctrl.providerMiddlewareFunction(domain, req, res, next, end)
+
+  function next() {
+    t.ok(false, 'next should not be called')
+    t.end()
+  }
+
+  function end(reason) {
+    t.ok(reason, 'error thrown')
+    t.equal(reason.code, 1, 'Auth error returned')
+    t.ok(equal(ctrl._getPermissions(otherDomain), expected), 'should have no permissions now')
+    t.ok(equal(ctrl._getPermissions(domain), expected), 'should have permissions still')
+    t.end()
+  }
+
+})
+
+test('revokePermissions on own permission deletes that permission.', async (t) => { })
+
