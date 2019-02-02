9f015352f2071764b55040a80e5f17dab8394721
Dan Finlay <dan@danfinlay.com>
2019-02-01 16:21:31 -0800

    Began factoring to jessie

--- index.js
+++ index.js
@@ -1,47 +1,40 @@
-const ObservableStore = require('obs-store')
-const equal = require('fast-deep-equal')
-const clone = require('clone')
+const ObservableStore = require('obs-store');
+const equal = require('fast-deep-equal');
+const clone = require('clone');
 
 
 const UNAUTHORIZED_ERROR = {
   message: 'Unauthorized to perform action',
   code: 1,
-}
+};
 const METHOD_NOT_FOUND = {
   code: -32601,
   message: 'Method not found',
-}
+};
 
 // TODO: This error code needs standardization:
 const USER_REJECTED_ERROR = {
   code: 5,
   message: 'User rejected the request.',
-}
+};
 
-class JsonRpcCapabilities {
+function createJsonRpcCapabilities ({ safeMethods = [], restrictedMethods = {}, initState = {}, methods = {}, methodPrefix = '', requestUserApproval}) {
 
-  constructor({ safeMethods = [], restrictedMethods = {}, initState = {}, methods = {}, methodPrefix = '', requestUserApproval}) {
-    this.safeMethods = safeMethods
-    this.restrictedMethods = restrictedMethods
-    this.methods = methods
-    this.requestUserApproval = requestUserApproval
+  const that = {};
 
-    this.internalMethods = {}
-    this.internalMethods[`${methodPrefix}getPermissions`] = this.getPermissionsMiddleware.bind(this)
-    this.internalMethods[`${methodPrefix}requestPermissions`] = this.requestPermissionsMiddleware.bind(this)
-    this.internalMethods[`${methodPrefix}grantPermissions`] = this.grantPermissionsMiddleware.bind(this)
-    this.internalMethods[`${methodPrefix}revokePermissions`] = this.revokePermissionsMiddleware.bind(this)
-    // TODO: Freeze internal methods object.
+  that.safeMethods = safeMethods;
+  that.restrictedMethods = restrictedMethods;
+  that.methods = methods;
+  that.requestUserApproval = requestUserApproval;
 
-    this.store = new ObservableStore(initState || {})
-    this.memStore = new ObservableStore({
-      permissionsRequests: [],
-    })
-  }
+  that.store = new ObservableStore(initState || {});
+  that.memStore = new ObservableStore({
+    permissionsRequests: [],
+  });
 
-  serialize () {
-    return this.store.getState()
-  }
+  that.serialize = function () {
+    return that.store.getState();
+  };
 
   /*
    * Returns a nearly json-rpc-engine compatible method.
@@ -56,121 +49,121 @@ class JsonRpcCapabilities {
    * @param {callback} next - A function to pass the responsibility of handling the request down the json-rpc-engine middleware stack.
    * @param {callback} end - A function to stop traversing the middleware stack, and reply immediately with the current `res`. Can be passed an Error object to return an error.
    */
-  providerMiddlewareFunction (domain, req, res, next, end) {
-    const methodName = req.method
+  that.providerMiddlewareFunction = function (domain, req, res, next, end) {
+    const methodName = req.method;
 
     // skip registered safe/passthrough methods.
-    if (this.safeMethods.includes(methodName)) {
-      return next()
+    if (that.safeMethods.includes(methodName)) {
+      return next();
     }
 
     // handle internal methods before any restricted methods.
-    if (methodName in this.internalMethods) {
-      return this.internalMethods[methodName](domain, req, res, next, end)
+    if (methodName in that.internalMethods) {
+      return that.internalMethods[methodName](domain, req, res, next, end);
     }
 
     // Traverse any permission delegations
-    let permission
+    let permission;
     try {
-      permission = this._getPermission(domain, methodName)
+      permission = that.getPermission(domain, methodName);
     } catch (err) {
       res.error = {
         message: err.message,
         code: 1,
-      }
-      return end(res.error)
+      };
+      return end(res.error);
     }
 
     if (!permission) {
-      res.error = UNAUTHORIZED_ERROR
-      return end(UNAUTHORIZED_ERROR)
+      res.error = UNAUTHORIZED_ERROR;
+      return end(UNAUTHORIZED_ERROR);
     }
 
-    this._executeMethod(domain, req, res, next, end)
-  }
+    that.executeMethod(domain, req, res, next, end);
+  };
 
-  _executeMethod(domain, req, res, next, end) {
-    const methodName = req.method
-    const permission = this._getPermission(domain, methodName)
-    if (methodName in this.restrictedMethods
-       && typeof this.restrictedMethods[methodName].method === 'function') {
-      const restrictedMethod = this.restrictedMethods[methodName]
+  that.executeMethod = function (domain, req, res, next, end) {
+    const methodName = req.method;
+    const permission = that.getPermission(domain, methodName);
+    if (methodName in that.restrictedMethods
+       && typeof that.restrictedMethods[methodName].method === 'function') {
+      const restrictedMethod = that.restrictedMethods[methodName];
 
       // Support static caveat:
       if (permission.caveats) {
-        const statics = permission.caveats.filter(c => c.type === 'static')
+        const statics = permission.caveats.filter(c => c.type === 'static');
 
         if (statics.length > 0) {
-          res.result = statics[statics.length - 1].value
-          return end()
+          res.result = statics[statics.length - 1].value;
+          return end();
         }
       }
 
-      return this.restrictedMethods[methodName].method(req, res, next, end)
+      return that.restrictedMethods[methodName].method(req, res, next, end);
     }
 
-    res.error = METHOD_NOT_FOUND
-    return end(METHOD_NOT_FOUND)
-  }
+    res.error = METHOD_NOT_FOUND;
+    return end(METHOD_NOT_FOUND);
+  };
 
-  _getPermissions (domain) {
-    const { domains = {} } = this.store.getState()
+  that.getPermissions = function (domain) {
+    const { domains = {} } = that.store.getState();
     if (domain in domains) {
-      const { permissions } = domains[domain]
-      return permissions
+      const { permissions } = domains[domain];
+      return permissions;
     }
-    return {}
-  }
+    return {};
+  };
 
   /*
    * Get the parent-most permission granting the requested domain's method permission.
    */
-  _getPermission (domain, method) {
+  that.getPermission = function (domain, method) {
     // TODO: Aggregate & Enforce Caveats at each step.
     // https://w3c-ccg.github.io/ocap-ld/#caveats
 
-    let permissions = this._getPermissions(domain)
+    let permissions = that.getPermissions(domain);
 
     while (permissions && method in permissions) {
       if ('grantedBy' in permissions[method]) {
-        permissions = this._getPermissions(permissions[method].grantedBy)
+        permissions = that.getPermissions(permissions[method].grantedBy);
       } else {
-        return permissions[method]
+        return permissions[method];
       }
     }
 
-    return undefined
-  }
+    return undefined;
+  };
 
   /*
    * Get the permission for that domain and method, not following grantedBy links.
    */
-  _getPermissionUnTraversed (domain, method) {
+  that.getPermissionUnTraversed = function (domain, method) {
     // TODO: Aggregate & Enforce Caveats at each step.
     // https://w3c-ccg.github.io/ocap-ld/#caveats
 
-    let permissions = this._getPermissions(domain)
+    let permissions = that.getPermissions(domain);
     if (permissions && method in permissions) {
-      return permissions[method]
+      return permissions[method];
     }
 
-    return undefined
-  }
+    return undefined;
+  };
 
 
-  get _permissions () {
-    const perms = this.memStore.getState().permissions
-    return perms || {}
-  }
+  that.getPermissions = function () {
+    const perms = that.memStore.getState().permissions;
+    return perms || {};
+  };
 
-  get _permissionsRequests () {
-    const reqs = this.memStore.getState().permissionsRequests
-    return reqs || {}
-  }
+  that.getPermissionsRequests = function () {
+    const reqs = that.memStore.getState().permissionsRequests;
+    return reqs || {};
+  };
 
-  set _permissionsRequests (permissionsRequests) {
-    this.memStore.updateState({ permissionsRequests })
-  }
+  that.setPermissionsRequests = function (permissionsRequests) {
+    that.memStore.updateState({ permissionsRequests });
+  };
 
   /*
    * Used for granting a new set of permissions,
@@ -178,79 +171,79 @@ class JsonRpcCapabilities {
    *
    * @param {object} permissions - An object describing the granted permissions.
    */
-  grantNewPermissions (domain, permissions, res, end) {
+  that.grantNewPermissions = function (domain, permissions, res, end) {
     // Remove any matching requests from the queue:
-    this._permissionsRequests = this._permissionsRequests.filter((request) => {
-      const sameDomain = request.domain === domain
-      const samePerms = equal(Object.keys(request.options), Object.keys(permissions))
-      return !(sameDomain && samePerms)
-    })
+    that.permissionsRequests = that.getPermissionsRequests.filter((request) => {
+      const sameDomain = request.domain === domain;
+      const samePerms = equal(Object.keys(request.options), Object.keys(permissions));
+      return !(sameDomain && samePerms);
+    });
 
     // Update the related permission objects:
-    this._setPermissionsFor(domain, permissions)
-    res.result = this._getPermissions(domain)
-    end()
-  }
+    that.setPermissionsFor(domain, permissions);
+    res.result = that.getPermissions(domain);
+    end();
+  };
 
-  get _domains () {
-    const { domains } = this.store.getState()
-    return domains || {}
-  }
+  that.getDomains = function () {
+    const { domains } = that.store.getState();
+    return domains || {};
+  };
 
-  set _domains (domains) {
-    this.store.updateState({ domains })
-  }
+  that.setDomains = function (domains) {
+    that.store.updateState({ domains });
+  };
 
-  _getDomainSettings (domain) {
-    const domains = this._domains
+  that.getDomainSettings = function (domain) {
+    const domains = that.getDomains;
 
     // Setup if not yet existent:
     if (!(domain in domains)) {
-      domains[domain] = { permissions: {} }
+      domains[domain] = { permissions: {} };
     }
 
-    return domains[domain]
-  }
+    return domains[domain];
+  };
 
-  _setDomain (domain, domainSettings) {
-    const domains = this._domains
-    domains[domain] = domainSettings
-    const state = this.store.getState()
-    state.domains = domains
-    this.store.putState(state)
-  }
+  that.setDomain = function (domain, domainSettings) {
+    const domains = that.getDomains;
+    domains[domain] = domainSettings;
+    const state = that.store.getState();
+    state.domains = domains;
+    that.store.putState(state);
+  };
 
-  _setPermissionsFor (domainName, newPermissions) {
-    const domain = this._getDomainSettings(domainName)
+  that.setPermissionsFor = function (domainName, newPermissions) {
+    const domain = that.getDomainSettings(domainName);
 
-    const { permissions } = domain
+    const { permissions } = domain;
 
     for (let key in newPermissions) {
-      permissions[key] = newPermissions[key]
+      permissions[key] = newPermissions[key];
     }
 
-    domain.permissions = permissions
-    this._setDomain(domainName, domain)
-  }
+    domain.permissions = permissions;
+    that.setDomain(domainName, domain);
+  };
 
-  _removePermissionsFor (domainName , permissionsToRemove) {
-    const domain = this._getDomainSettings(domainName)
+  that.removePermissionsFor = function (domainName , permissionsToRemove) {
+    const domain = that.getDomainSettings(domainName);
 
-    const { permissions } = domain
+    const { permissions } = domain;
 
     permissionsToRemove.forEach((key) => {
-      delete permissions[key]
-    })
+      delete permissions[key];
+    });
 
-    domain.permissions = permissions
-    this._setDomain(domainName, domain)
-  }
+    domain.permissions = permissions;
+    that.setDomain(domainName, domain);
+  };
 
-  getPermissionsMiddleware (domain, req, res, next, end) {
-    const permissions = this._getPermissions(domain)
-    res.result = permissions
-    end()
-  }
+  that.getPermissionsMiddleware = function (domain, req, res, next, end) {
+    const permissions = that.getPermissions(domain);
+    res.result = permissions;
+    end();
+  };
 
   /*
    * The capabilities middleware function used for requesting additional permissions from the user.
@@ -259,98 +252,109 @@ class JsonRpcCapabilities {
    * @param {Array} req.params - The JSON RPC formatted params array.
    * @param {object} req.params[0] - An object of the requested permissions.
    */
-  requestPermissionsMiddleware (domain, req, res, next, end) {
+  that.requestPermissionsMiddleware = function (domain, req, res, next, end) {
 
     // TODO: Validate permissions request
-    const options = req.params[0]
-    const requests = this._permissionsRequests
+    const options = req.params[0];
+    const requests = that.getPermissionsRequests;
     requests.push({
       domain,
       options,
-    })
-    this._permissionsRequests = requests
+    });
+    that.setPermissionsRequests(requests);
 
-    if (!this.requestUserApproval) {
-      res.result = 'Request submitted, no user approval callback provided.'
-      return end()
+    if (!that.requestUserApproval) {
+      res.result = 'Request submitted, no user approval callback provided.';
+      return end();
     }
 
-    this.requestUserApproval(domain, options)
+    that.requestUserApproval(domain, options)
     // TODO: Allow user to pass back an object describing
     // the approved permissions, allowing user-customization.
     .then((approved) => {
 
       if (!approved) {
-        res.error = USER_REJECTED_ERROR
-        return end(USER_REJECTED_ERROR)
+        res.error = USER_REJECTED_ERROR;
+        return end(USER_REJECTED_ERROR);
       }
 
       // If user approval is boolean, the request is wholly approved
       if (typeof approved === 'boolean') {
-        return this.grantNewPermissions(domain, options, res, end)
+        return that.grantNewPermissions(domain, options, res, end);
       }
 
       // If user approval is different, use it as the permissions:
-      this.grantNewPermissions(domain, approved, res, end)
+      that.grantNewPermissions(domain, approved, res, end);
     })
     .catch((reason) => {
-      res.error = reason
-      return end(reason)
-    })
-  }
+      res.error = reason;
+      return end(reason);
+    });
+  };
 
-  grantPermissionsMiddleware (domain, req, res, next, end) {
+  that.grantPermissionsMiddleware = function (domain, req, res, next, end) {
     // TODO: Validate params
-    const [ assignedDomain, requestedPerms ] = req.params
-    const perms = this._getPermissions(domain)
-    const assigned = this._getPermissions(assignedDomain)
-    const newlyGranted = {}
+    const [ assignedDomain, requestedPerms ] = req.params;
+    const perms = that.getPermissions(domain);
+    const assigned = that.getPermissions(assignedDomain);
+    const newlyGranted = {};
     for (const methodName in requestedPerms) {
-      const perm = this._getPermission(domain, methodName)
+      const perm = that.getPermission(domain, methodName);
       if (perm) {
         const newPerm = {
           date: Date.now(),
           grantedBy: domain,
-        }
-        assigned[methodName] = newPerm
-        newlyGranted[methodName] = newPerm
+        };
+        assigned[methodName] = newPerm;
+        newlyGranted[methodName] = newPerm;
       } else {
-        res.error = UNAUTHORIZED_ERROR
-        return end(UNAUTHORIZED_ERROR)
+        res.error = UNAUTHORIZED_ERROR;
+        return end(UNAUTHORIZED_ERROR);
       }
     }
 
-    this._setPermissionsFor(assignedDomain, assigned)
-    res.result = newlyGranted
-    end()
-  }
+    that.setPermissionsFor(assignedDomain, assigned);
+    res.result = newlyGranted;
+    end();
+  };
 
-  revokePermissionsMiddleware (domain, req, res, next, end) {
+  that.revokePermissionsMiddleware = function (domain, req, res, next, end) {
     // TODO: Validate params
-    const [ assignedDomain, requestedPerms ] = req.params
-    const perms = this._getPermissions(domain)
-    const assigned = this._getPermissions(assignedDomain)
-    const newlyRevoked = []
+    const [ assignedDomain, requestedPerms ] = req.params;
+    const perms = that.getPermissions(domain);
+    const assigned = that.getPermissions(assignedDomain);
+    const newlyRevoked = [];
     for (const methodName in requestedPerms) {
-      const perm = this._getPermissionUnTraversed(assignedDomain, methodName)
+      const perm = that.getPermissionUnTraversed(assignedDomain, methodName);
       if (perm &&
           // Grantors can revoke what they have granted:
          ((perm.grantedBy && perm.grantedBy === domain)
           // Domains can revoke their own permissions:
           || (assignedDomain === domain))) {
 
-        newlyRevoked.push(methodName)
+        newlyRevoked.push(methodName);
       } else {
-        res.error = UNAUTHORIZED_ERROR
-        return end(UNAUTHORIZED_ERROR)
+        res.error = UNAUTHORIZED_ERROR;
+        return end(UNAUTHORIZED_ERROR);
       }
     }
 
-    this._removePermissionsFor(assignedDomain, newlyRevoked)
-    res.result = newlyRevoked
-    end()
-  }
+    that.removePermissionsFor(assignedDomain, newlyRevoked);
+    res.result = newlyRevoked;
+    end();
+  };
+
+  that.internalMethods = {};
+  that.internalMethods[`${methodPrefix}getPermissions`] = that.getPermissionsMiddleware.bind(that);
+  that.internalMethods[`${methodPrefix}requestPermissions`] = that.requestPermissionsMiddleware.bind(that);
+  that.internalMethods[`${methodPrefix}grantPermissions`] = that.grantPermissionsMiddleware.bind(that);
+  that.internalMethods[`${methodPrefix}revokePermissions`] = that.revokePermissionsMiddleware.bind(that);
+  // TODO: Freeze internal methods object.
+
+  return that;
 }
 
-module.exports = JsonRpcCapabilities
+module.exports = function (opts) {
+  return createJsonRpcCapabilities(opts);
+};
 
