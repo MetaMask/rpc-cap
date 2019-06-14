5d4bfeb3a368e49e4bbc3a6a005fa772644c4d09
Dan Finlay <dan@danfinlay.com>
2019-06-14 15:45:58 -0700

    Fix json-rpc signature

--- src/@types/json-rpc-engine.d.ts
+++ src/@types/json-rpc-engine.d.ts
@@ -1,7 +1,7 @@
 import { JsonRpcRequest, JsonRpcResponse, JsonRpcError } from 'json-rpc-capabilities-middleware/src/@types/json-rpc-2';
 
 type JsonRpcEngineEndCallback = (error?: JsonRpcError<any>) => void;
-type JsonRpcEngineNextCallback = (returnFlightCallback?: (res: JsonRpcResponse<any>) => void) => void;
+type JsonRpcEngineNextCallback = (returnFlightCallback?: (done: () => void) => void) => void;
 
 interface JsonRpcMiddleware {
   (
