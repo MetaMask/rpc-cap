303b4beeab85944da23c87204c712d38abe3e3f4
Whymarrh Whitby <whymarrh.whitby@gmail.com>
2020-02-18 12:06:28 -0330

    Clean up ESLint config (#76)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,8 @@
 module.exports = {
-
-  parser: '@typescript-eslint/parser',
-
-  plugins: ['@typescript-eslint'],
-
-  ignorePatterns: ['scripts/'],
-
+  root: true,
   extends: [
     '@metamask/eslint-config',
-    '@metamask/eslint-config/config/typescript',
     '@metamask/eslint-config/config/nodejs',
+    '@metamask/eslint-config/config/typescript',
   ],
-}
+};
