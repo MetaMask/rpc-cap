ccaab4c4e14c0a4bb25fd007d4e6f330d206e58a
Erik Marks <rekmarks@protonmail.com>
2021-05-06 15:14:33 -0700

    STILL MORE build script fixup

--- scripts/copyDistTypes.sh
+++ scripts/copyDistTypes.sh
@@ -4,15 +4,9 @@ set -e
 set -x
 set -o pipefail
 
-function main {
-  local NO_DIST_MSG="Please run \`yarn build:typescript\` before continuing."
+if [[ ! -d "dist" ]]; then
+  printf "Error: %s\\n" "Please run \`yarn build:typescript\` before continuing." >&2
+  exit 1
+fi
 
-  if [[ ! -d "dist" ]]; then
-    printf "ERROR: %s\\n" "${NO_DIST_MSG}" >&2
-    exit 1
-  fi
-
-  cp -r "src/types" "dist/types"
-}
-
-main
+cp -r "src/types" "dist/types"
