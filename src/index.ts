83b4fcc49f5c76719a8ca5b5eccae466e8352755
Erik Marks <rekmarks@protonmail.com>
2021-05-06 14:55:49 -0700

    Move index.ts to /src

--- src/index.ts
+++ src/index.ts
@@ -27,23 +27,23 @@ import type {
   RpcCapDomainRegistry,
   OriginString,
   PermittedJsonRpcMiddleware,
-} from './src/types';
+} from './types';
 
 import {
   CaveatFunction,
   CaveatFunctionGenerator,
   caveatFunctions,
-} from './src/caveats';
+} from './caveats';
 
 import {
   unauthorized,
   userRejectedRequest,
   methodNotFound,
-} from './src/errors';
+} from './errors';
 
-import { OcapLdCapability, OcapLdCaveat } from './src/types/ocap-ld';
+import { OcapLdCapability, OcapLdCaveat } from './types/ocap-ld';
 
-export { CaveatTypes } from './src/caveats';
+export { CaveatTypes } from './caveats';
 
 export type AnnotatedJsonRpcEngine = {
   domain?: OriginString;
